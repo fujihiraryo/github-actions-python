b0d905585680d3886412eadf4677467e6e8286e0
藤平燎 <fujihiraryo@team-lab.com>
2020-10-07 02:37:32 +0900

    失敗例(test)

--- src/test/test_sample.py
+++ src/test/test_sample.py
@@ -1,5 +1,9 @@
-from src.main.sample import say_hello
+from src.main.sample import say_hello, plus
 
 
 def test_say_hello():
     assert say_hello() == "hello"
+
+
+def test_plus():
+    assert plus(1, 1) == 0
